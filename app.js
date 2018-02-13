6585fd1df24070e4ea6ac847148faf2f9f3b747d
Clark Walker <303walker@gmail.com>
2018-02-13 14:09:23 -0700

    update

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const path = require('path');
+
 // set view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 // for css
 app.use(express.static('public'));
 
+//get routes
 const home = require("./routes/api.js")
+app.use("/", home)
 
 app.listen(port, () => {
   console.log("listening on port", port);
